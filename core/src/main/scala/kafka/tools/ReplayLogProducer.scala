7de9b4c03bfcfa7cda4505bb9306e2316a2a96af
benstopford <benstopford@gmail.com>
2015-10-15 16:49:26 +0100

    KAFKA-2647: this jira was removed as no longer needed. Got this going but it's too similar to mirrormaker to be any use.

--- core/src/main/scala/kafka/tools/ReplayLogProducer.scala
+++ core/src/main/scala/kafka/tools/ReplayLogProducer.scala
@@ -6,7 +6,7 @@
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,68 +17,118 @@
 
 package kafka.tools
 
-import joptsimple.OptionParser
-import java.util.concurrent.{Executors, CountDownLatch}
 import java.util.Properties
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.atomic.AtomicLong
+
+import joptsimple.OptionParser
 import kafka.consumer._
-import kafka.utils.{ToolsUtils, CommandLineUtils, Logging, ZkUtils}
-import kafka.api.OffsetRequest
-import org.apache.kafka.clients.producer.{ProducerRecord, KafkaProducer, ProducerConfig}
+import kafka.utils.{CommandLineUtils, Logging, ToolsUtils}
+import org.apache.kafka.clients.consumer.internals.NoOpConsumerRebalanceListener
+import org.apache.kafka.clients.consumer.{ConsumerConfig, ConsumerRebalanceListener, ConsumerRecords, KafkaConsumer}
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerConfig, ProducerRecord}
+import scala.collection.JavaConversions._
 
-object ReplayLogProducer extends Logging {
 
-  private val GroupId: String = "replay-log-producer"
+trait Configuration {
+  val numMessages: Int
+  val numThreads: Int
+  val inputTopic: String
+  val outputTopic: String
+  val isSync: Boolean
+  val reportingInterval: Int
+  val producerProps: Properties
+  val consumerProps: Properties
+}
+
+class ReplayLogProducer extends Logging {
+  var latch: CountDownLatch = _
+  var threadList = List[ReplayThread]()
+  val globalCount = new AtomicLong()
 
   def main(args: Array[String]) {
-    val config = new Config(args)
-
-    val executor = Executors.newFixedThreadPool(config.numThreads)
-    val allDone = new CountDownLatch(config.numThreads)
-
-    // if there is no group specified then avoid polluting zookeeper with persistent group data, this is a hack
-    ZkUtils.maybeDeletePath(config.zkConnect, "/consumers/" + GroupId)
-    Thread.sleep(500)
-
-    // consumer properties
-    val consumerProps = new Properties
-    consumerProps.put("group.id", GroupId)
-    consumerProps.put("zookeeper.connect", config.zkConnect)
-    consumerProps.put("consumer.timeout.ms", "10000")
-    consumerProps.put("auto.offset.reset", OffsetRequest.SmallestTimeString)
-    consumerProps.put("fetch.message.max.bytes", (1024*1024).toString)
-    consumerProps.put("socket.receive.buffer.bytes", (2 * 1024 * 1024).toString)
-    val consumerConfig = new ConsumerConfig(consumerProps)
-    val consumerConnector: ConsumerConnector = Consumer.create(consumerConfig)
-    val topicMessageStreams = consumerConnector.createMessageStreams(Predef.Map(config.inputTopic -> config.numThreads))
-    var threadList = List[ZKConsumerThread]()
-    for ((topic, streamList) <- topicMessageStreams)
-      for (stream <- streamList)
-        threadList ::= new ZKConsumerThread(config, stream)
-
-    for (thread <- threadList)
-      thread.start
-
-    threadList.foreach(_.shutdown)
-    consumerConnector.shutdown
+    new ReplayLogProducer()
+      .run(new ConfigBuilder(args))
+      .awaitTerminationAndClose()
   }
 
-  class Config(args: Array[String]) {
+  def run(config: Configuration): ReplayLogProducer = {
+    run(config, new NoOpConsumerRebalanceListener())
+  }
+
+  def run(config: Configuration, rebalanceListener: ConsumerRebalanceListener): ReplayLogProducer = {
+    latch = new CountDownLatch(config.numThreads)
+
+    for (a <- 1 to config.numThreads) {
+      threadList ::= new ReplayThread(config, latch, globalCount, rebalanceListener)
+    }
+    for (thread <- threadList)
+      thread.start
+    this
+  }
+
+  def awaitTerminationAndClose(): Unit = {
+    latch.await
+    threadList.foreach(_.shutdown)
+  }
+
+  class ReplayThread(config: Configuration, latch: CountDownLatch, globalCounter: AtomicLong, rebalanceListener: ConsumerRebalanceListener) extends Thread with Logging {
+    val producer = new KafkaProducer[Array[Byte], Array[Byte]](config.producerProps)
+    val consumer = new KafkaConsumer[Array[Byte], Array[Byte]](config.consumerProps)
+
+    override def run() {
+      consumer.subscribe(List(config.inputTopic), rebalanceListener)
+      var localCounter = 0
+      while (!done) {
+        info("Thread %s for partitions %s completed poll returning %s records at global msg counter %s".format(Thread.currentThread.getId, consumer.assignment(), consumer.subscription(), globalCounter.get))
+
+        for (record <- consumer.poll(100)) {
+          if (!done) {
+            try {
+              val response = producer.send(new ProducerRecord[Array[Byte], Array[Byte]](config.outputTopic, record.key(), record.value()))
+              if (config.isSync)
+                response.get()
+            } catch {
+              case ie: Exception => error("Skipping this message", ie)
+            }
+            globalCounter.incrementAndGet()
+            localCounter += 1
+          }
+          if (localCounter % config.reportingInterval == 0)
+            info("Thread %s for partitions %s completed poll at global msg counter %s".format(Thread.currentThread.getId, consumer.assignment(), globalCounter.get))
+        }
+      }
+      latch.countDown
+      info("Thread %s completed processing having sent %s messages".format(Thread.currentThread().getId, localCounter))
+    }
+
+    def done: Boolean = {
+      globalCounter.get >= config.numMessages
+    }
+
+    def shutdown() {
+      producer.close
+      consumer.close
+    }
+
+    override def info(msg: => String): Unit = {
+      println(msg)
+    }
+  }
+
+
+  class ConfigBuilder(args: Array[String]) extends Configuration {
+
     val parser = new OptionParser
-    val zkConnectOpt = parser.accepts("zookeeper", "REQUIRED: The connection string for the zookeeper connection in the form host:port. " +
-      "Multiple URLS can be given to allow fail-over.")
-      .withRequiredArg
-      .describedAs("zookeeper url")
-      .ofType(classOf[String])
-      .defaultsTo("127.0.0.1:2181")
     val brokerListOpt = parser.accepts("broker-list", "REQUIRED: the broker list must be specified.")
       .withRequiredArg
       .describedAs("hostname:port")
       .ofType(classOf[String])
-    val inputTopicOpt = parser.accepts("inputtopic", "REQUIRED: The topic to consume from.")
+    val inputTopicOpt = parser.accepts("input-topic", "REQUIRED: The topic to consume from.")
       .withRequiredArg
       .describedAs("input-topic")
       .ofType(classOf[String])
-    val outputTopicOpt = parser.accepts("outputtopic", "REQUIRED: The topic to produce to")
+    val outputTopicOpt = parser.accepts("output-topic", "REQUIRED: The topic to produce to")
       .withRequiredArg
       .describedAs("output-topic")
       .ofType(classOf[String])
@@ -96,71 +146,55 @@ object ReplayLogProducer extends Logging {
       .withRequiredArg
       .describedAs("size")
       .ofType(classOf[java.lang.Integer])
-      .defaultsTo(5000)
-    val propertyOpt = parser.accepts("property", "A mechanism to pass properties in the form key=value to the producer. " +
+      .defaultsTo(0)
+    val producerPropsOpt = parser.accepts("producer-properties", "A mechanism to pass properties in the form key=value to the producer. " +
       "This allows the user to override producer properties that are not exposed by the existing command line arguments")
       .withRequiredArg
       .describedAs("producer properties")
       .ofType(classOf[String])
+    val consumerPropsOpt = parser.accepts("consumer-properties", "A mechanism to pass properties in the form key=value to the consumer. " +
+      "This allows the user to override consumer properties that are not exposed by the existing command line arguments")
+      .withRequiredArg
+      .describedAs("consumer properties")
+      .ofType(classOf[String])
+    val genericProsOpt = parser.accepts("generic-properties", "A mechanism to pass properties in the form key=value to both the producer and the consumer. " +
+      "This allows the user to override producer and consumer properties that are not exposed by the existing command line arguments")
+      .withRequiredArg
+      .describedAs("generic properties")
+      .ofType(classOf[String])
+
     val syncOpt = parser.accepts("sync", "If set message send requests to the brokers are synchronously, one at a time as they arrive.")
 
-    val options = parser.parse(args : _*)
-    
+    val options = parser.parse(args: _*)
+
     CommandLineUtils.checkRequiredArgs(parser, options, brokerListOpt, inputTopicOpt)
 
-    val zkConnect = options.valueOf(zkConnectOpt)
     val brokerList = options.valueOf(brokerListOpt)
-    ToolsUtils.validatePortOrDie(parser,brokerList)
+    ToolsUtils.validatePortOrDie(parser, brokerList)
     val numMessages = options.valueOf(numMessagesOpt).intValue
     val numThreads = options.valueOf(numThreadsOpt).intValue
     val inputTopic = options.valueOf(inputTopicOpt)
     val outputTopic = options.valueOf(outputTopicOpt)
     val reportingInterval = options.valueOf(reportingIntervalOpt).intValue
     val isSync = options.has(syncOpt)
-    import scala.collection.JavaConversions._
-    val producerProps = CommandLineUtils.parseKeyValueArgs(options.valuesOf(propertyOpt))
+
+    val genericProps = CommandLineUtils.parseKeyValueArgs(options.valuesOf(genericProsOpt))
+
+    val producerProps = CommandLineUtils.parseKeyValueArgs(options.valuesOf(producerPropsOpt))
+    producerProps.putAll(genericProps)
     producerProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, brokerList)
     producerProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.ByteArraySerializer")
     producerProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.ByteArraySerializer")
+
+    val consumerProps = CommandLineUtils.parseKeyValueArgs(options.valuesOf(consumerPropsOpt))
+    consumerProps.putAll(genericProps)
+    consumerProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, brokerList)
+    consumerProps.put(ConsumerConfig.GROUP_ID_CONFIG, "test-group-" + System.currentTimeMillis())
+    consumerProps.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false")
+    consumerProps.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest")
+    consumerProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.ByteArrayDeserializer")
+    consumerProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.ByteArrayDeserializer")
+    consumerProps.put(ConsumerConfig.FETCH_MAX_WAIT_MS_CONFIG, "0") //ensure we have no temporal batching
   }
 
-  class ZKConsumerThread(config: Config, stream: KafkaStream[Array[Byte], Array[Byte]]) extends Thread with Logging {
-    val shutdownLatch = new CountDownLatch(1)
-    val producer = new KafkaProducer[Array[Byte],Array[Byte]](config.producerProps)
-
-    override def run() {
-      info("Starting consumer thread..")
-      var messageCount: Int = 0
-      try {
-        val iter =
-          if(config.numMessages >= 0)
-            stream.slice(0, config.numMessages)
-          else
-            stream
-        for (messageAndMetadata <- iter) {
-          try {
-            val response = producer.send(new ProducerRecord[Array[Byte],Array[Byte]](config.outputTopic,
-                                            messageAndMetadata.key(), messageAndMetadata.message()))
-            if(config.isSync) {
-              response.get()
-            }
-            messageCount += 1
-          }catch {
-            case ie: Exception => error("Skipping this message", ie)
-          }
-        }
-      }catch {
-        case e: ConsumerTimeoutException => error("consumer thread timing out", e)
-      }
-      info("Sent " + messageCount + " messages")
-      shutdownLatch.countDown
-      info("thread finished execution !" )
-    }
-
-    def shutdown() {
-      shutdownLatch.await
-      producer.close
-    }
-
-  }
 }
---
+++ core/src/test/scala/kafka/tools/ReplayLogProducerTest.scala
@@ -0,0 +1,207 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package kafka.tools
+
+import java.util
+import java.util.Properties
+import java.util.concurrent.TimeUnit._
+import java.util.concurrent.{TimeUnit, CountDownLatch}
+import java.util.concurrent.atomic.AtomicInteger
+
+import kafka.integration.KafkaServerTestHarness
+import kafka.server.KafkaConfig
+import kafka.utils.{ZkUtils, TestUtils}
+import org.apache.kafka.clients.consumer._
+import org.apache.kafka.clients.consumer.internals.NoOpConsumerRebalanceListener
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerConfig, ProducerRecord}
+import org.apache.kafka.common.TopicPartition
+import org.apache.kafka.common.protocol.SecurityProtocol
+import org.junit.{After, Assert, Test}
+
+import scala.collection.JavaConversions._
+
+class ReplayLogProducerTest extends KafkaServerTestHarness {
+
+  @Test
+  def shouldPushMessagesFromInboundToOutboundTopic(): Unit = {
+    val config = new kafka.tools.Configuration {
+      override val numMessages: Int = 10
+      override val consumerProps: Properties = consumerProperties
+      override val producerProps: Properties = producerProperties
+      override val isSync: Boolean = false
+      override val outputTopic: String = "out"
+      override val inputTopic: String = "in"
+      override val numThreads: Int = 1
+      override val reportingInterval: Int = 1
+    }
+    val dataGenerator = new KafkaProducer[Array[Byte], Array[Byte]](config.producerProps)
+    val consumerPartitionAllocated = new CountDownLatch(1)
+
+    //Given
+    val replayer = new ReplayLogProducer().run(config, evenBalanceListener(consumerPartitionAllocated))
+
+    consumerPartitionAllocated.await()
+
+    //When
+    for (range <- 1 to config.numMessages) {
+      dataGenerator.send(record(config.inputTopic)).get()
+    }
+    dataGenerator.flush()
+
+    replayer.awaitTerminationAndClose()
+
+    //Then
+    Assert.assertEquals(config.numMessages, countRecords(config.outputTopic))
+  }
+
+  @Test
+  def shouldPushMessagesUsingMultipleThreads(): Unit = {
+
+    val config = new kafka.tools.Configuration {
+      override val numMessages: Int = 500
+      override val consumerProps: Properties = consumerProperties
+      override val producerProps: Properties = producerProperties
+      override val isSync: Boolean = false
+      override val outputTopic: String = "out"
+      override val inputTopic: String = "in"
+      override val numThreads: Int = 11
+      override val reportingInterval: Int = 1
+    }
+    val dataGenerator = new KafkaProducer[Array[Byte], Array[Byte]](config.producerProps)
+    val evenBalance = new CountDownLatch(config.numThreads)
+
+    //Create topic with one partition per thread
+    TestUtils.createTopic(zkClient, config.inputTopic, config.numThreads, 1, servers, new Properties())
+
+    //Start the log replayer
+    val replayer = new ReplayLogProducer().run(config, evenBalanceListener(evenBalance))
+
+    //Wait for each consumer (thread) to be assigned a single partition
+    evenBalance.await(15, SECONDS)
+    if (evenBalance.getCount > 0)
+      fail("Consumers did not reach even balance within timeout")
+
+    //Populate the inbound topic
+
+    for (range <- 1 to config.numMessages)
+      dataGenerator.send(record(config.inputTopic)).get()
+    dataGenerator.flush()
+
+    //Await completion
+    replayer.awaitTerminationAndClose()
+
+    //Ensure messages were copied to outbound topic
+    Assert.assertEquals(config.numMessages, countRecords(config.outputTopic))
+  }
+
+
+  @Test
+  def shouldLimitByNumberMessagesParameter(): Unit = {
+    val config = new kafka.tools.Configuration {
+      override val numMessages: Int = 10
+      override val consumerProps: Properties = consumerProperties
+      override val producerProps: Properties = producerProperties
+      override val isSync: Boolean = false
+      override val outputTopic: String = "out"
+      override val inputTopic: String = "in"
+      override val numThreads: Int = 1
+      override val reportingInterval: Int = 1
+    }
+    val dataGenerator = new KafkaProducer[Array[Byte], Array[Byte]](config.producerProps)
+    val consumerPartitionAllocated = new CountDownLatch(1)
+
+    //Given
+    val replayer = new ReplayLogProducer().run(config, evenBalanceListener(consumerPartitionAllocated))
+
+    consumerPartitionAllocated.await()
+
+    //When
+    for (range <- 1 to config.numMessages + 10) {
+      dataGenerator.send(record(config.inputTopic)).get()
+    }
+    dataGenerator.flush()
+
+    replayer.awaitTerminationAndClose()
+
+    //Then
+    Assert.assertEquals(config.numMessages, countRecords(config.outputTopic))
+  }
+
+  def evenBalanceListener(evenBalance: CountDownLatch): ConsumerRebalanceListener = {
+    new ConsumerRebalanceListener() {
+      override def onPartitionsAssigned(partitions: util.Collection[TopicPartition]): Unit = {
+        if (partitions.size() == 1)
+          evenBalance.countDown()
+      }
+
+      override def onPartitionsRevoked(partitions: util.Collection[TopicPartition]): Unit = {}
+    }
+  }
+
+  def countRecords(topic: String): Int = {
+    val consumer = consumerInDifferentGroup(topic)
+    val timeout: Long = System.currentTimeMillis() + 3000
+    var count = 0
+    while (System.currentTimeMillis() < timeout) {
+      val poll: ConsumerRecords[Array[Byte], Array[Byte]] = consumer.poll(100)
+      for (r <- poll) {
+        count += 1
+      }
+    }
+    count
+  }
+
+  def consumerInDifferentGroup(topic: String): KafkaConsumer[Array[Byte], Array[Byte]] = {
+    val props = new Properties()
+    props.putAll(consumerProperties)
+    props.put(ConsumerConfig.GROUP_ID_CONFIG, "testObserver" + System.currentTimeMillis())
+    props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
+    val consumer = new KafkaConsumer[Array[Byte], Array[Byte]](props)
+    consumer.subscribe(List(topic))
+    consumer
+  }
+
+  def record(topic: String): ProducerRecord[Array[Byte], Array[Byte]] = {
+    new ProducerRecord[Array[Byte], Array[Byte]](topic, "value".getBytes())
+  }
+
+  def consumerProperties: Properties = {
+    val propConsumer: Properties = new Properties()
+    propConsumer.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:" + servers(1).boundPort(SecurityProtocol.PLAINTEXT))
+    propConsumer.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.ByteArrayDeserializer")
+    propConsumer.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.ByteArrayDeserializer")
+    propConsumer.put(ConsumerConfig.GROUP_ID_CONFIG, "replay-log-producer-shared-group")
+    //        propConsumer.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
+    propConsumer
+  }
+
+  def producerProperties: Properties = {
+    val propProducer: Properties = new Properties()
+    propProducer.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:" + servers(1).boundPort(SecurityProtocol.PLAINTEXT))
+    propProducer.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.ByteArraySerializer")
+    propProducer.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.ByteArraySerializer")
+    propProducer
+  }
+
+  @After
+  override def tearDown() {
+    super.tearDown()
+  }
+
+  override def generateConfigs(): Seq[KafkaConfig] = TestUtils.createBrokerConfigs(2, zkConnect, false).map(KafkaConfig.fromProps(_, new Properties()))
+}
