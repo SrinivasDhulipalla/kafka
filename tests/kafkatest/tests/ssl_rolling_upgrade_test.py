8ea156d6434bfc6a57ba05d13d366e72b3fa0d46
Ben Stopford <benstopford@gmail.com>
2015-11-24 10:05:02 +0000

    KAFKA-2771 - increased sleep, reduced throughput, added logging to produce consume

--- tests/kafkatest/tests/ssl_rolling_upgrade_test.py
+++ tests/kafkatest/tests/ssl_rolling_upgrade_test.py
@@ -32,7 +32,7 @@ class TestRollingSSLUpgrade(ProduceConsumeValidateTest):
 
     def setUp(self):
         self.topic = "test_topic"
-        self.producer_throughput = 1000
+        self.producer_throughput = 100
         self.num_producers = 1
         self.num_consumers = 1
         self.zk = ZookeeperService(self.test_context, num_nodes=1)
@@ -65,17 +65,17 @@ class TestRollingSSLUpgrade(ProduceConsumeValidateTest):
         self.kafka.open_port('SSL')
         for node in self.kafka.nodes:
             self.kafka.stop_node(node)
-            time.sleep(5)
+            time.sleep(15)
             self.kafka.start_node(node)
-            time.sleep(5)
+            time.sleep(15)
 
         # Roll cluster to disable PLAINTEXT port
         self.kafka.close_port('PLAINTEXT')
         for node in self.kafka.nodes:
             self.kafka.stop_node(node)
-            time.sleep(5)
+            time.sleep(15)
             self.kafka.start_node(node)
-            time.sleep(5)
+            time.sleep(15)
 
 
     def add_ssl_port(self):
@@ -84,9 +84,9 @@ class TestRollingSSLUpgrade(ProduceConsumeValidateTest):
 
         for node in self.kafka.nodes:
             self.kafka.stop_node(node)
-            time.sleep(5)
+            time.sleep(15)
             self.kafka.start_node(node)
-            time.sleep(5)
+            time.sleep(15)
 
 
     def test_rolling_upgrade_phase_one(self):
