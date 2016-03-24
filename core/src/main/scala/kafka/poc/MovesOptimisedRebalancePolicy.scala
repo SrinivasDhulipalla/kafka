8589d22828a8d75922e982c2a72e413c8746a4c1
Ben Stopford <benstopford@gmail.com>
2016-03-24 07:23:59 +0000

    refactored - but rough

--- core/src/main/scala/kafka/poc/MovesOptimisedRebalancePolicy.scala
+++ core/src/main/scala/kafka/poc/MovesOptimisedRebalancePolicy.scala
@@ -12,23 +12,32 @@ class MovesOptimisedRebalancePolicy extends RabalancePolicy {
     val partitions = collection.mutable.Map(replicasForPartitions.toSeq: _*) //todo deep copy
     val replicaFilter = new ReplicaFilter(brokers, partitions)
 
-
-    def racksFor(p: TopicAndPartition) = {
-      brokers.filter(broker =>
-        partitions.get(p).get
-          .contains(broker.id)
-      ).map(_.rack.get)
-    }
-
     //Ensure partitions are fully replicated
     for (partition <- partitions.keys) {
       def replicationFactor = replicationFactors.get(partition.topic).get
       def replicasForP = partitions.get(partition).get
 
       while (replicasForP.size < replicationFactor) {
-        val leastLoadedBrokers = replicaFilter.leastLoadedBrokersDownranking(racksFor(partition))
+        val leastLoadedBrokers = replicaFilter.leastLoadedBrokersDownranking(replicaFilter.racksFor(partition))
         val leastLoadedButNoExistingReplica = leastLoadedBrokers.filterNot(replicasForP.toSet).last
         partitions.put(partition, replicasForP :+ leastLoadedButNoExistingReplica)
+      }
+    }
+
+    // Optimise for replica fairness across racks
+    for(aboveParRack <- replicaFilter.aboveParRacks()){
+      for(replicaToMove <- replicaFilter.weightedReplicasFor(aboveParRack)){
+          for(belowParRack <-  replicaFilter.belowParRacks){
+            for (broker <- replicaFilter.leastLoadedBrokers(belowParRack)) {
+              val partition = replicaToMove.topicAndPartition
+              var replicas = partitions.get(partition).get
+              //remove the old broker from the replicas list
+              replicas = replicas.filter(_ == replicaToMove.broker)
+              //add the new broker to the replicas list
+              replicas = replicas :+ broker
+              partitions.put(partition, replicas)
+            }
+          }
       }
     }
 
