38f476d71c3b942285ceb7893696bc426e40e6bb
Ben Stopford <benstopford@gmail.com>
2016-03-27 07:41:13 +0100

    Core algorithm is now complete (although performance is knowingly unacceptable)

--- core/src/main/scala/kafka/poc/MovesOptimisedRebalancePolicy.scala
+++ core/src/main/scala/kafka/poc/MovesOptimisedRebalancePolicy.scala
@@ -21,6 +21,7 @@ class MovesOptimisedRebalancePolicy extends RabalancePolicy {
         val leastLoadedBrokers = cluster.leastLoadedBrokersDownranking(cluster.racksFor(partition))
         val leastLoadedButNoExistingReplica = leastLoadedBrokers.filterNot(replicasForP.toSet).last
         partitionsMap.put(partition, replicasForP :+ leastLoadedButNoExistingReplica)
+        println(s"Additional replica was created on broker [$leastLoadedButNoExistingReplica] for under-replicated partition [$partition].")
       }
     }
 
@@ -46,7 +47,7 @@ class MovesOptimisedRebalancePolicy extends RabalancePolicy {
       for (leader <- cluster.leadersOn(aboveParRack)) {
         //ensure rack is still above par (could be optimised out as above)
         if (cluster.leaderFairness.aboveParRacks().contains(aboveParRack)) {
-          //check to see if the partition has a non-leader repilca on below par racks
+          //check to see if the partition has a non-leader replica on below par racks
           for (replica <- partitionsMap.get(leader).get.drop(1)) {
             if (cluster.brokersOn(cluster.leaderFairness.belowParRacks()).contains(replica)) {
               //if so, switch leadership
@@ -56,16 +57,49 @@ class MovesOptimisedRebalancePolicy extends RabalancePolicy {
         }
       }
     }
+
+    //Step 3.1: Optimise for replica fairness across brokers
+    for (aboveParBroker <- cluster.replicaFairness.aboveParBrokers) {
+      for (replicaToMove <- cluster.weightedReplicasFor(aboveParBroker)) {
+        for (belowParBroker <- cluster.replicaFairness.belowParBrokers) {
+          val partition = replicaToMove.topicAndPartition
+          val brokerFrom: Int = replicaToMove.broker
+          val brokerTo: Int = belowParBroker.id
+          move(partition, brokerFrom, brokerTo, partitionsMap)
+        }
+      }
+    }
+
+    //Step 3.2: Optimise for leader fairness across brokers
+    //consider leaders on above par brokers
+    for (aboveParBroker <- cluster.leaderFairness.aboveParBrokers()) {
+      //for each leader (could be optimised to be for(n) where n is the number we expect to move)
+      for (leader <- cluster.leadersOn(aboveParBroker)) {
+        //ensure broker is still above par (could be optimised out as above)
+        if (cluster.leaderFairness.aboveParBrokers().contains(aboveParBroker)) {
+          //check to see if the partition has a non-leader replica on below par brokers
+          for (replica <- partitionsMap.get(leader).get.drop(1)) {
+            if (cluster.leaderFairness.belowParBrokers().map(_.id).contains(replica)) {
+              //if so, switch leadership
+              makeLeader(leader, replica, partitionsMap)
+            }
+          }
+        }
+      }
+    }
+
     partitionsMap
   }
 
   def makeLeader(tp: TopicAndPartition, toPromote: Int, partitionsMap: collection.mutable.Map[TopicAndPartition, Seq[Int]]): Unit = {
     var replicas = partitionsMap.get(tp).get
+    var currentLead = replicas(0)
     //remove old
     replicas = replicas.filter(_ != toPromote)
     //put first
     replicas = Seq(toPromote) ++ replicas
     partitionsMap.put(tp, replicas)
+    println(s"Leadership moved brokers: [$currentLead -> $toPromote] for partition $tp")
   }
 
   def move(tp: TopicAndPartition, from: Int, to: Int, partitionsMap: collection.mutable.Map[TopicAndPartition, Seq[Int]]): Unit = {
@@ -75,5 +109,6 @@ class MovesOptimisedRebalancePolicy extends RabalancePolicy {
     //add new
     replicas = replicas :+ to
     partitionsMap.put(tp, replicas)
+    println(s"Partition $tp was moved from broker [$from] to [$to]")
   }
 }
