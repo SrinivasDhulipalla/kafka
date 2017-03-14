1e38cbeff3f279d2572182d87c4e933dfa9343e3
Ben Stopford <benstopford@gmail.com>
2017-03-14 12:09:38 +0000

    KIP-101: Consolidated LeaderEpoch IntegrationTests

--- core/src/test/scala/unit/kafka/server/epoch/LeaderEpochIntegrationTest.scala
+++ core/src/test/scala/unit/kafka/server/epoch/LeaderEpochIntegrationTest.scala
@@ -16,19 +16,31 @@
   */
 package unit.kafka.server.epoch
 
+import kafka.admin.AdminUtils
+import kafka.server.KafkaConfig._
+import kafka.server.epoch.OffsetsForLeaderEpoch._
+import kafka.server.epoch.{LeaderEpochFetcher, PartitionEpoch}
 import kafka.server.{KafkaConfig, KafkaServer}
 import kafka.utils.TestUtils._
 import kafka.utils.{Logging, TestUtils}
 import kafka.zk.ZooKeeperTestHarness
-import org.apache.kafka.clients.producer.ProducerRecord
-import org.apache.kafka.common.TopicPartition
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
+import org.apache.kafka.common.metrics.Metrics
+import org.apache.kafka.common.protocol.Errors._
 import org.apache.kafka.common.serialization.StringSerializer
+import org.apache.kafka.common.utils.SystemTime
+import org.apache.kafka.common.{Node, TopicPartition}
+import org.junit.Assert._
 import org.junit.{After, Before, Test}
+import unit.kafka.server.epoch.util.TestSender
+
+import scala.collection.JavaConverters._
 
 class LeaderEpochIntegrationTest extends ZooKeeperTestHarness with Logging {
   var brokers: Seq[KafkaServer] = null
   val topic1 = "foo"
   val topic2 = "bar"
+  var producer: KafkaProducer[Array[Byte], Array[Byte]] = null
 
   @Before
   override def setUp() {
@@ -40,6 +52,8 @@ class LeaderEpochIntegrationTest extends ZooKeeperTestHarness with Logging {
   @After
   override def tearDown() {
     brokers.foreach(_.shutdown())
+    if(producer != null)
+      producer.close()
     super.tearDown()
   }
 
@@ -73,6 +87,51 @@ class LeaderEpochIntegrationTest extends ZooKeeperTestHarness with Logging {
     waitUntilTrue(() => messagesHaveLeaderEpoch(expectedLeaderEpoch, 4), "Broker logs should be identical")
   }
 
+  @Test
+  def shouldSendLeaderEpochRequestAndGetAResponse(): Unit = {
+
+    //3 brokers, put partition on 100/101 and then pretend to be 102
+    brokers = (100 to 102).map { id => createServer(fromProps(createBrokerConfig(id, zkConnect))) }
+    AdminUtils.createOrUpdateTopicPartitionAssignmentPathInZK(zkUtils, topic1, Map(
+      0 -> Seq(100),
+      1 -> Seq(101)
+    ))
+
+    //Send messages equally to the two partitions
+    producer = createNewProducer(getBrokerListStrFromServers(brokers), retries = 5, acks = -1)
+    (0 until 100).foreach { _ =>
+      producer.send(new ProducerRecord(topic1, 0, null, "test0".getBytes))
+      producer.send(new ProducerRecord(topic1, 1, null, "test1".getBytes))
+    }
+    producer.flush()
+
+    val fetcher = new LeaderEpochFetcher(sender(brokers(0)))
+
+    val tp1 = new TopicPartition(topic1, 0)
+    val tp2 = new TopicPartition(topic1, 2)
+    val epochsRequested = Set(new PartitionEpoch(tp1, 5), new PartitionEpoch(tp2, 7))
+
+    //When
+    val responses = fetcher.fetchLeaderEpochs(epochsRequested)
+
+    //Then end offset should be defined as 0 for tp1
+    assertEquals(0, responses(tp1).endOffset)
+
+    assertTrue(responses(tp2).hasError)
+    assertEquals(REPLICA_NOT_AVAILABLE.code(), responses(tp2).error)
+    assertEquals(UNDEFINED_OFFSET, responses(tp2).endOffset)
+
+    assertEquals(2, responses.size)
+  }
+
+  //TODO more here please
+
+  def sender(broker: KafkaServer): TestSender = {
+    val endPoint = broker.metadataCache.getAliveBrokers.find(_.id == 100).get.getBrokerEndPoint(broker.config.interBrokerListenerName)
+    val destinationNode = new Node(endPoint.id, endPoint.host, endPoint.port)
+    new TestSender(destinationNode, broker.config, new Metrics(), new SystemTime())
+  }
+
   def waitForEpochChangeTo(topic: String, partition: Int, epoch: Int): Boolean = {
     TestUtils.waitUntilTrue(() => {
       brokers(0).metadataCache.getPartitionInfo(topic, partition) match {
@@ -92,11 +151,10 @@ class LeaderEpochIntegrationTest extends ZooKeeperTestHarness with Logging {
           if (segment.read(minOffset, None, Integer.MAX_VALUE) == null) {
             false
           } else {
-            val deepEntries = segment.read(minOffset, None, Integer.MAX_VALUE).records.deepEntries.iterator()
-            scala.collection.JavaConversions.asScalaIterator(deepEntries).forall { msg =>
-              info("tp:" + tp + " offset:" + msg.offset + " => LeaderEpoch: " + msg.record().leaderEpoch())
-              expectedLeaderEpoch == msg.record().leaderEpoch()
-            }
+            segment.read(minOffset, None, Integer.MAX_VALUE)
+              .records.deepEntries.iterator().asScala.forall (
+              expectedLeaderEpoch == _.record().leaderEpoch()
+            )
           }
         }
       }
